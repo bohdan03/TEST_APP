23ebb7886d7760f949a3fd6cdb97e6e5a716c90b
OK MAC <okmac@VictoryMac.local>
2015-02-05 04:01:20 -0800

    Initial Commit

---
+++ OurNet_Test/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  OurNet_Test
+//
+//  Created by OK MAC on 2/5/15.
+//  Copyright (c) 2015 DoubleDigital. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ OurNet_Test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OurNet_Test
+//
+//  Created by OK MAC on 2/5/15.
+//  Copyright (c) 2015 DoubleDigital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
